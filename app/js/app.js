f8dda4c698216bfe0004c11269393392b5cabb71
Brian best <brian.brianb64@gmail.com>
2014-11-05 12:01:36 -0400

    adding in polymer

--- app/js/app.js
+++ app/js/app.js
@@ -21,5 +21,33 @@ var app = (function(document, $) {
 
 	'use strict';
 	app.init();
+    //Socket stuff
+//----------------------------
+    var socket = io();
+
+    $('form').submit(function(){
+        socket.emit('chat message', $('#message').val());
+        $('#message').val('');
+        return false;
+    });
+
+    socket.on('chat message', function(msg){
+        console.log(msg);
+        make_message(msg);
+        $('.message_holder').append($('<p>').text(msg));
+    });
+//----------------------------
+
 
 })();
+
+
+
+$('.cast_player').click(function(){
+    $(this).toggleClass('expand_bio');
+});
+
+
+function make_message(message){
+
+}
