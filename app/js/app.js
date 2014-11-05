2a2867de1a5f9e0858873c838f392d77d4d6be33
Brian best <brian.brianb64@gmail.com>
2014-11-05 16:51:43 -0400

    had polymer working, just broke it

--- app/js/app.js
+++ app/js/app.js
@@ -1,26 +1,7 @@
-var app = (function(document, $) {
-
-	'use strict';
-	var docElem = document.documentElement,
-
-		_userAgentInit = function() {
-			docElem.setAttribute('data-useragent', navigator.userAgent);
-		},
-		_init = function() {
-			$(document).foundation();
-			_userAgentInit();
-		};
-
-	return {
-		init: _init
-	};
-
-})(document, jQuery);
-
 (function() {
 
 	'use strict';
-	app.init();
+	//app.init();
     //Socket stuff
 //----------------------------
     var socket = io();
@@ -34,7 +15,7 @@ var app = (function(document, $) {
     socket.on('chat message', function(msg){
         console.log(msg);
         make_message(msg);
-        $('.message_holder').append($('<p>').text(msg));
+        $('.message_holder').append($('<rph-msg>').text(msg));
     });
 //----------------------------
 
@@ -47,7 +28,3 @@ $('.cast_player').click(function(){
     $(this).toggleClass('expand_bio');
 });
 
-
-function make_message(message){
-
-}
