0307e82a1b18ca5d6cc56dd6cb3254765e217345
Brian best <brian.brianb64@gmail.com>
2014-11-07 09:24:06 -0400

    polymer working, making db

--- app/js/app.js
+++ app/js/app.js
@@ -1,20 +1,41 @@
 (function() {
 
-	'use strict';
+	//'use strict';
 	//app.init();
     //Socket stuff
+    $(document).foundation();
+
+    var winHeight = $( window ).height();
+    winHeight = winHeight - 75;
+    $('.message_holder').css('height',winHeight);
+
+
 //----------------------------
     var socket = io();
 
-    $('form').submit(function(){
+    $('#msg_form').submit(function(){
         socket.emit('chat message', $('#message').val());
         $('#message').val('');
         return false;
     });
 
+    $('#create_char').submit(function(){
+        var new_char = {
+            user_name : $('#user_name').val(),
+            char_name : $('#char_name').val(),
+            age : $('#age').val(),
+            gender : $('#gender').val(),
+            bio : $('#bio').val()
+        };
+        socket.emit('new character', new_char);
+
+        return false;
+    });
+
     socket.on('chat message', function(msg){
         console.log(msg);
-        make_message(msg);
+
+        $('.message_holder').scrollTop = $('.message_holder').scrollHeight;
         $('.message_holder').append($('<rph-msg>').text(msg));
     });
 //----------------------------
@@ -28,3 +49,33 @@ $('.cast_player').click(function(){
     $(this).toggleClass('expand_bio');
 });
 
+
+//Switches tabs
+var tabs = document.querySelector('paper-tabs');
+var currTab = 'online_tab';//set defualt tab
+
+tabs.addEventListener('core-select', function() {
+    switch (tabs.selected){
+        case "online":
+            $('.' + currTab).css('display','none');
+            currTab = 'online_tab';
+            $('.' + currTab).css('display','block');
+            break;
+        case "cast":
+            $('.' + currTab).css('display','none');
+            currTab = 'cast_tab';
+            $('.' + currTab).css('display','block');
+            break;
+        case "story":
+            $('.' + currTab).css('display','none');
+            currTab = 'story_tab';
+            $('.' + currTab).css('display','block');
+            break;
+        case "setting":
+            $('.' + currTab).css('display','none');
+            currTab = 'setting_tab';
+            $('.' + currTab).css('display','block');
+            break;
+    }
+    console.log("Selected: " + tabs.selected);
+});
